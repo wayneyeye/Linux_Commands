b33c85cb07f8a5fc4253454ac30550b470419e75
yewenhe0904 <yewenhe0904@gmail.com>
2017-06-03 18:41:36 -0500

    mod tar-bz.sh

--- tar-bz.sh
+++ tar-bz.sh
@@ -4,3 +4,8 @@ tar -zcv -f test_tar.tar.gz *.md
 tar -ztv -f test_tar.tar.gz 
 # decompress file
 tar -zxv -f test_tar.tar.gz  -C test_tar_dest/
+
+
+
+#copy r - means a temporary cache in RAM
+tar -cvf - /etc | tar -xvf -
