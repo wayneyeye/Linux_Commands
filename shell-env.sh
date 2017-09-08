6771bcc853b573a6f3e5f05973baca5c15b08a4c
yewenhe0904 <yewenhe0904@gmail.com>
2017-09-08 13:45:18 -0500

    bash env, vim 1

---
+++ shell-env.sh
@@ -0,0 +1,14 @@
+#display env vars
+printenv
+
+#display env vars and user defined vars
+set|less
+
+#check alias
+alias
+
+#use the parent envPATH in the subprocesses
+export PATH
+
+#activate .bashrc
+source .bashrc
