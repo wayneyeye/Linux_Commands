6095ba5ea38bd084cd73d33ac1ff4fba970c564a
yewenhe0904 <yewenhe0904@gmail.com>
2017-09-14 09:54:20 -0500

    update tar-bz.sh

--- tar-bz.sh
+++ tar-bz.sh
@@ -1,11 +1,26 @@
+# tar = tape archive
+# c create tarball, x extract from tarball, t list, r append files to the, v verbose mode
+# C change to the directory
+
 # compress using gz
 tar -zcv -f test_tar.tar.gz *.md
+
 # lookup compressed file
 tar -ztv -f test_tar.tar.gz 
+
 # decompress file
 tar -zxv -f test_tar.tar.gz  -C test_tar_dest/
 
-
-
-#copy r - means a temporary cache in RAM
+#copy - means a temporary cache in RAM
 tar -cvf - /etc | tar -xvf -
+
+#pipeline tar
+find filename -name 'file-A' | tar cf - --files-from=- | gzip
+#or
+find filename -name 'file-A' | tar cf - -T | gzip
+#or
+find filename -name 'file-A' | tar czf - -T #use gzip
+#or
+find filename -name 'file-A' | tar cjf - -T #use bzip2
+
+
