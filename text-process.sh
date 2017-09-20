d39a58cdb6c5672ba1b6a42fb511426b806f2f7c
yewenhe0904 <yewenhe0904@gmail.com>
2017-09-20 14:37:43 -0500

    add cut paste join

--- text-process.sh
+++ text-process.sh
@@ -15,4 +15,22 @@ ls -l /usr/bin|sort -nk 5 |head
 # sort by multiple field
 ls -l|sort --key=1,1 --key=5n
 
+# uniq -- delete duplicated rows
+# sort -u
+# -d only output duplicated lines
+# -i ignore cases
+
+# cut delete part of the text
+# -f field list
+# -c characters cut -c 7-11
+# -d specify delimiter
+# --complement extract the rest
+ls -li|cut -c 1-5 --complement|cut -d ' ' -f 5
+
+# paste column bind two files
+
+# join 
+
+# comm
+
 
