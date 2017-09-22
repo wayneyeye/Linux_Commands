3e7d1291bc7bd37b8c2fe07401a29181307babb6
yewenhe0904 <yewenhe0904@gmail.com>
2017-09-22 12:44:18 -0500

    add format_text

---
+++ format_text.sh
@@ -0,0 +1,11 @@
+# Formating Tools
+# nl -- set line numbers
+ls -li | nl
+# -n set number alignment
+# -b a -->set number for all rows
+
+# fold -- set width of a line
+ls -li | fold -w 30 -s
+# -s for not spliting the last word in the line
+
+
