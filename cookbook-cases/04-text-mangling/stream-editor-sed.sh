7332fb3c13df552d7a52e9e33678e787b0a70657
yewenhe0904 <yewenhe0904@gmail.com>
2018-04-18 09:40:31 -0500

    finish sed

--- cookbook-cases/04-text-mangling/stream-editor-sed.sh
+++ cookbook-cases/04-text-mangling/stream-editor-sed.sh
@@ -1,3 +1,7 @@
+
+## use single quotes to AVOID Evaluating
+## use double quotes to Evaluate shell variables
+
 # use sed to replace pattern(s) in a file
 ls -l | sed 's/pi/wayne/g' # s for substitute, g for global find and replace
 
@@ -7,4 +11,11 @@ ls -l | sed 's/\s\+/\t/g' #notice the \ before special chars
 # removing blank lines
 echo -e "this is the first line\nthis is the second line\n\nthis is the fourth line" |sed '/^$/d' 
 
-# edit and replace file inplace by adding -i
+# edit and replace file inplace by adding -i.bak
+sed -i.bak 's/\b[0-9]\{3\}\b/NUMBER/g' sed_test.txt # use \b to indicate word boundaries 
+
+# string match notation
+cat sed_test.txt.bak |sed 's/\b\w\+\b/[&]/g'  # use \b to indicate word boundaries 
+
+# substring match & notation/replace
+cat sed_test.txt.bak | sed 's/\([0-9]\)/*\1*/g'  # use \b to indicate word boundaries 
